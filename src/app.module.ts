f8f6f7c477b2dfcf88d366926db7ab89ddb561ed
Diego Muñoz <d.munozmauro@hotmail.com>
2024-07-30 11:27:17 -0400

    prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { FilesModule } from './files/files.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 
 @Module({
@@ -29,7 +30,8 @@ import { AuthModule } from './auth/auth.module';
     SeedModule,
     FilesModule,
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    AuthModule
+    AuthModule,
+    MessagesWsModule
   ],
   controllers: [],
   providers: [],
