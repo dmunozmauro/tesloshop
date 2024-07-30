2bd9d0670d771fd5a5698bf13ba278ebe3b7a916
dmunozmauro <d.munozmauro@hotmail.com>
2024-07-30 11:38:07 -0400

    prueba merge

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-
+// prueba para el merge
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
